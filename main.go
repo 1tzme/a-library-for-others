57c4ba924f49588d727416022f5fac6fac6085e5
1tzme <zakir.itsme@gmail.com>
2025-06-03 15:36:33 +0500

    add parser

--- main.go
+++ main.go
@@ -1,5 +1,31 @@
 package main
 
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"a-library-for-others/parser"
+)
+
 func main() {
-	
+	file, err := os.Open("example.csv")
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		return
+	}
+	defer file.Close()
+
+	var csvparser parser.CSVParser = MyCSVParser{}
+
+	for {
+		line, err := csvparser.ReadLine(file)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			fmt.Println("Error reading line:", err)
+			return
+		}
+	}
 }
